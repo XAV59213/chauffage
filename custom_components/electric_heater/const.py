b1881b7c1c8f7e7a859e7c33221fe052b0c19771
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-12-07 18:26:36 +0100

    Update const.py

--- custom_components/electric_heater/const.py
+++ custom_components/electric_heater/const.py
@@ -1,15 +1,12 @@
 DOMAIN = "electric_heater"
-
 CENTRAL = "central"
 ROOM = "room"
-
 PRESET_COMFORT = "confort"
 PRESET_COMFORT_M1 = "confort_-1"
 PRESET_COMFORT_M2 = "confort_-2"
 PRESET_ECO = "eco"
 PRESET_HORS_GEL = "hors_gel"
 PRESET_OFF = "off"
-
 PRESETS = [
     PRESET_COMFORT,
     PRESET_COMFORT_M1,
@@ -18,14 +15,12 @@ PRESETS = [
     PRESET_HORS_GEL,
     PRESET_OFF,
 ]
-
 OFFSET = {
     PRESET_COMFORT: 0.0,
     PRESET_COMFORT_M1: -1.0,
     PRESET_COMFORT_M2: -2.0,
     PRESET_ECO: -3.5,
 }
-
 FIL_PILOTE_PAYLOAD = {
     PRESET_COMFORT: {"fil_pilote": "comfort"},
     PRESET_COMFORT_M1: {"fil_pilote": "comfort_minus_1"},
@@ -34,5 +29,5 @@ FIL_PILOTE_PAYLOAD = {
     PRESET_HORS_GEL: {"fil_pilote": "frost_protection"},
     PRESET_OFF: {"fil_pilote": "off"},
 }
-
 DEFAULT_FROST_TEMP = 7.0
+DEFAULT_MQTT_BASE_TOPIC = "zigbee2mqtt"  # ← Ajoute cette ligne (topic base standard pour Z2M)
